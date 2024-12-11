4b1280f275b5857743c546516d4ce0785822b39f
Ramalingam-Srinivasan <Ramalingam-Srinivasan@gmail.com>
2024-12-11 16:05:04 +0530

    dataframe introduction

--- MySpark/HelloSpark.py
+++ MySpark/HelloSpark.py
@@ -1,7 +1,7 @@
 import sys
 from pyspark.sql import *
 from lib.logger import Log4j
-from lib.utils import get_spark_app_config
+from lib.utils import get_spark_app_config,load_survey_df
 
 if __name__ == "__main__":
     conf = get_spark_app_config()
@@ -13,8 +13,15 @@ if __name__ == "__main__":
 
     logger = Log4j(spark)
 
+    if len(sys.argv)!=2:
+        logger.error("usage:hello spark <filename>")
+        sys.exit(-1)
+
     logger.info("Starting HelloSpark")
     #conf_out = spark.sparkContext.getConf()
     #logger.info(conf_out)
+
+    survey_df = load_survey_df(spark,sys.argv[1])
+    survey_df.show()
     logger.info("Finished HelloSpark")
     #spark.stop()
--- MySpark/lib/utils.py
+++ MySpark/lib/utils.py
@@ -1,5 +1,6 @@
 import configparser
 from pyspark import SparkConf
+import sys
 
 def get_spark_app_config():
     spark_conf = SparkConf()
@@ -9,3 +10,9 @@ def get_spark_app_config():
     for (key, val) in config.items("SPARK_APP_CONFIGS"):
         spark_conf.set(key, val)
     return spark_conf
+
+def load_survey_df(spark,data_file):
+   return  spark.read \
+        .option("header", "true") \
+        .option("inferSchema", "true") \
+        .csv(data_file)
