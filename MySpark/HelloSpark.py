ab8f7c66a918fcfbda7616a14c461d4863de75fc
Ramalingam-Srinivasan <Ramalingam-Srinivasan@gmail.com>
2024-12-11 15:13:42 +0530

    added spark.conf and utils.py

--- MySpark/HelloSpark.py
+++ MySpark/HelloSpark.py
@@ -1,21 +1,20 @@
 import sys
 from pyspark.sql import *
 from lib.logger import Log4j
+from lib.utils import get_spark_app_config
 
 if __name__ == "__main__":
-
+    conf = get_spark_app_config()
     spark = SparkSession \
         .builder \
-        .appName("HelloSpark") \
-        .master("local[2]") \
+       .config(conf=conf) \
         .getOrCreate()
     spark.sparkContext.setLogLevel("INFO")
 
     logger = Log4j(spark)
 
-
-
     logger.info("Starting HelloSpark")
-
+    #conf_out = spark.sparkContext.getConf()
+    #logger.info(conf_out)
     logger.info("Finished HelloSpark")
     #spark.stop()
--- MySpark/lib/utils.py
+++ MySpark/lib/utils.py
@@ -1,11 +1,11 @@
-from pyspark import SparkConf
 import configparser
+from pyspark import SparkConf
 
-def get_spark_app_configs():
+def get_spark_app_config():
     spark_conf = SparkConf()
     config = configparser.ConfigParser()
-    config.read("spark_conf")
+    config.read("spark.conf")
 
-    for(key,val) in config.items("SPARK_APP_CONFIGS"):
-        spark_conf.set(key,val)
+    for (key, val) in config.items("SPARK_APP_CONFIGS"):
+        spark_conf.set(key, val)
     return spark_conf
